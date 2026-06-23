38152af5129409d18466f8e838438b48dbc9096c
Rafael Pereira de Oliveira <rafaelpdoadm@gmail.com>
2026-06-23 17:24:33 -0300

    solutions to the proposed exercises in the course

---
+++ SQL-Consultas/resolucao_exercicios2.sql
@@ -0,0 +1,9 @@
+-- 1) Retornar a média de Notas dos Alunos em história --
+
+-- 2) Retornar as informações dos alunos cujo Nome começa com 'A' --
+
+-- 3) Buscar apenas os alunos que fazem aniversário em fevereiro --
+
+-- 4) Realizar uma consulta que calcula a idade dos Alunos --
+
+-- 5) Retornar se o aluno está ou não aprovado. Aluno é considerado aprovado se a sua nota foi igual ou maior que 6 --
