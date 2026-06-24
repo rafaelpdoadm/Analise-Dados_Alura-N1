eb83ff404bc8c3cba9a12a2795a3b41bb071e62f
Rafael Pereira de Oliveira <rafaelpdoadm@gmail.com>
2026-06-24 12:01:45 -0300

    update with the resolution to the test

--- SQL-Consultas/resolucao_exercicios2.sql
+++ SQL-Consultas/resolucao_exercicios2.sql
@@ -1,9 +1,27 @@
+-- Para a resulucao desses exercicios foi utilizada a DB 'sqlite' --
+
 -- 1) Retornar a média de Notas dos Alunos em história --
+SELECT AVG(nota) AS media
+FROM Notas
+WHERE id_disciplina = 2;
 
 -- 2) Retornar as informações dos alunos cujo Nome começa com 'A' --
+SELECT *
+FROM Alunos
+where nome_aluno LIKE 'A%';
 
 -- 3) Buscar apenas os alunos que fazem aniversário em fevereiro --
+SELECT *
+FROM Alunos
+WHERE STRFTIME('%m', data_nascimento) = '02';
 
 -- 4) Realizar uma consulta que calcula a idade dos Alunos --
+SELECT nome_aluno, data_nascimento, (STRFTIME('%Y', CURRENT_DATE) - STRFTIME('%Y', data_nascimento)) - (STRFTIME('%m-%d', CURRENT_DATE) < STRFTIME('%m-%d', data_nascimento)) AS idade
+FROM Alunos;
 
 -- 5) Retornar se o aluno está ou não aprovado. Aluno é considerado aprovado se a sua nota foi igual ou maior que 6 --
+SELECT id_aluno AS aluno, nota,
+CASE WHEN nota>= 6 THEN 'Aprovado'
+ELSE 'Reprovado'
+END AS Resultado
+FROM Notas;
